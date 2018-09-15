8494f5737a0a01103123a37fdf43d4be28eacfb7
breakinferno <1972952841@qq.com>
2018-09-15 16:53:37 +0800

    some demo

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const Koa = require('koa')
+const static = require('koa-static')
+const path = require('path')
+const fs = require('fs')
 const app = new Koa()
 const SDK = require('../sdk/index').sdk
-console.log(SDK);
+// console.log(SDK);
 // const axios = require('axios');
 const sdk = new SDK('ld2n7Myo+2rA/EYjrVeK4w==', 'BaH8uHyPTGz7WJAIB8IXIg==');
 
@@ -9,11 +12,48 @@ const urlHanlder = (url) => {
     return url === '/' ? 'send' : url.split('?')[0].slice(1);
 }
 
+// 解析上下文里node原生请求的POST参数
+function parsePostData( ctx ) {
+    return new Promise((resolve, reject) => {
+      try {
+        let postdata = "";
+        ctx.req.addListener('data', (data) => {
+          postdata += data
+        })
+        ctx.req.addListener("end",function(){
+        //   let parseData = parseQueryStr( postdata )
+        let parseData = JSON.parse(postdata).data
+          resolve( parseData )
+        })
+      } catch ( err ) {
+        reject(err)
+      }
+    })
+  }
+  
+  // 将POST请求参数字符串解析成JSON
+  function parseQueryStr( queryStr ) {
+    let queryData = {}
+    let queryStrList = queryStr.split('&')
+    console.log( queryStrList )
+    for (  let [ index, queryStr ] of querySt5rList.entries()  ) {
+      let itemList = queryStr.split('=')
+      queryData[ itemList[0] ] = decodeURIComponent(itemList[1])
+    }
+    return queryData
+  }
+  
+
 const send = async (ctx) => {
+    if (ctx.method !== 'POST') {
+        throw new Error()
+    }
+    let postData = await parsePostData( ctx )
+    const {target, user, content} = postData
     let [err, result] = await sdk.sendComment({
-        'targetId': '2323',
-        'userId': 'fuckfuck',
-        content: 'test eeste test',
+        'targetId': target,
+        'userId': user,
+        content,
         createdTime: Date.now(),
         updatedTime: Date.now(),
         extra: {
@@ -126,6 +166,7 @@ const setExtra = async (ctx) => {
     ctx.body = result.data;
 }
 
+app.use(static(path.join(__dirname, '../static')))
 
 
 app.use( async ( ctx ) => {
